42fee4e5fe38fba34d1b5c993c235a672edf2379
ahmadalanib <alnaib888@gmail.com>
2024-10-25 11:54:22 +0200

    Revoking Authentication Tokens

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -16,22 +16,25 @@ class AuthController extends Controller
     use ApiResponses;
     public function login(LoginUserRequest $request)
     {
-       $request->validated($request->all());    
-       if(!Auth::attempt($request->only('email', 'password'))){
-           return $this->errorResponse('Invalid credentials', 401);
-       }
-         $user =User::firstWhere('email', $request->email);
-         return $this->ok(
+        $request->validated($request->all());
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return $this->errorResponse('Invalid credentials', 401);
+        }
+        $user = User::firstWhere('email', $request->email);
+        return $this->ok(
             'Authenticated',
             [
                 'token' => $user->createToken('API authToken for' . $user->email)->plainTextToken,
-               
+
             ]
 
 
-         );
-      
+        );
     }
 
-
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return $this->ok('Logged out', []);
+    }
 }
--- app/Traits/ApiResponses.php
+++ app/Traits/ApiResponses.php
@@ -3,12 +3,12 @@ namespace App\Traits;
 
 trait ApiResponses{
 
-protected function ok($message,$data){
+protected function ok($message,$data=[]){
     return $this->successResponse($message,$data, 200);
 }
 
 
-protected function successResponse($message, $data,$statusCode = 200)
+protected function successResponse($message, $data=[],$statusCode = 200)
     {
         return response()->json([
             'data' => $data,
